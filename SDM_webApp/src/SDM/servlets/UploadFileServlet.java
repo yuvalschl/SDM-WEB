9d4f89b092aa684f3d5b02bcb76817b726e79877
yuvalschl <yuval.schl@gmail.com>
2020-10-06 21:04:45 +0300

    started working on file upload

---
+++ SDM_webApp/src/SDM/servlets/UploadFileServlet.java
@@ -0,0 +1,36 @@
+package SDM.servlets;
+
+import SDM.utils.ServletUtils;
+import com.google.gson.Gson;
+import logicSDM.AllZonesManager.AllZonesManager;
+import users.SingelUserEntry;
+import users.UserManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+public class UploadFileServlet extends HttpServlet{
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //returning JSON objects, not HTML
+        response.setContentType("application/json");
+        try (PrintWriter out = response.getWriter()) {
+            Gson gson = new Gson();
+            AllZonesManager zoneManager = ServletUtils.getAllZoneManager(getServletContext());
+            String json = gson.toJson(zoneManager);
+            out.println(json);
+            out.flush();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+}
