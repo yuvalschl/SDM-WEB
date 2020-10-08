7d81b0b2779d035a145bf13a28e8d4f7c8b5be7e
Dani <dani.zion87@gmail.com>
2020-10-08 17:39:25 +0300

    working on diffrence between owner and client on 3rd pg

--- SDM_webApp/web/pages/localStores/localStores.js
+++ SDM_webApp/web/pages/localStores/localStores.js
@@ -1,30 +1,31 @@
 var GET_ITEM_DATA = buildUrlWithContextPath("getItemData")
 
-var pickedup;
-
 $(document).ready(function() {
     ajaxItemTableData();
-    $( "#storesNameTable tbody tr" ).on( "click", function( event ) {
 
-        // get back to where it was before if it was selected :
-        if (pickedup != null) {
-            pickedup.css( "background-color", "#f8f9fa" );
-        }
-
-        $("#fillname").val($(this).find("td").eq(0).html());
-        $( this ).css( "background-color", "#a5e3f6" );
-
-        pickedup = $( this );
-    });
 });
 
+function checkIfCustomer() {
+   /* $.ajax({
+        url: GET_ITEM_DATA,
+        dataType: 'json',
+        data: "zonename=" + zone,
+        success: function (itemData){
+            updateTable(itemData)
+            console.log("table loaded")
+        },
+        error: function (errorInfo){
+            console.log("error while uploading file" + errorInfo)
+        }
+    })*/
+}
 function updateTableSingleEntry(index, itemInfo){
-    var itemID = itemInfo.itemdID
+    var itemID = itemInfo.itemID
     var itemName = itemInfo.itemName
     var sellBy = itemInfo.sellBy
     var numberOfStoresSelling = itemInfo.amountOfStoresSelling
     var averagePrice = itemInfo.averagePrice
-    var howManyItemSold = itemInfo.averagePriceOfOrders
+    var howManyItemSold = itemInfo.numberOfTimesItemWasSold
     $("#itemTable").append("<tr>" +
         "<td>" + itemID + "</td>" +
         "<td>" + itemName + "</td>" +
@@ -35,11 +36,15 @@ function updateTableSingleEntry(index, itemInfo){
         "</tr>");
 }
 
+function ajaxGetUser(){
+
+}
 function ajaxItemTableData(){
+    var zone = GetURLParameter("zonename");
     $.ajax({
         url: GET_ITEM_DATA,
-
         dataType: 'json',
+        data: "zonename=" + zone,
         success: function (itemData){
             updateTable(itemData)
             console.log("table loaded")
@@ -53,7 +58,6 @@ function updateTable(table){
     $("#tableBody").empty()
     $.each(table || [], updateTableSingleEntry)
 }
-
 function GetURLParameter(sParam) {
     var sPageURL = window.location.search.substring(1);
     var sURLVariables = sPageURL.split('&');
@@ -63,4 +67,4 @@ function GetURLParameter(sParam) {
             return sParameterName[1];
         }
     }
-}​
+}
