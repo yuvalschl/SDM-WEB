9abf3ecd580017ae07c7723e883e8e791c0cb3fb
Dani <dani.zion87@gmail.com>
2020-10-14 15:32:23 +0300

    working on adding discount to cart

--- SDM_webApp/web/pages/localStores/customerPage/placeOrder/approveOrder/order.js
+++
@@ -1,8 +0,0 @@
-class order {
-    constructor(stores, orderTotal, shippingTotal, totalItemCost) {
-        this._stores = stores;
-        this._orderTotal = orderTotal;
-        this._shippingTotal = shippingTotal;
-        this._totalItemCost = totalItemCost;
-    }
-}
